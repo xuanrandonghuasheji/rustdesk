e6470f2ab905964e8ce29ad5537e57bc1d8a4e95
xuanrandonghuasheji <956665818@qq.com>
2025-07-07 12:20:31 +0800

    修改替换API地址

--- src/common.rs
+++ src/common.rs
@@ -1004,7 +1004,7 @@ fn get_api_server_(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "https://admin.rustdesk.com".to_owned()
+    "http://149.88.87.191:21114".to_owned()
 }
 
 #[inline]
